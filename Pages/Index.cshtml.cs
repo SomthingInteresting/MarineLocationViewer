9b7933392147b60aaa916a1d42e43223684c2c8d
Quddus Rahman <qrahman115@gmail.com>
2023-08-22 10:58:31 +0100

    added markers to map

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,35 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MarineLocationViewer.Repositories;
+using System.Collections.Generic; // For List<>
+using System.Threading.Tasks; // For Task
 
-namespace MarineLocationViewer.Pages;
-
-public class IndexModel : PageModel
+namespace MarineLocationViewer.Pages
 {
-    private readonly MarineDataRepository _repository;
-
-    public string Message { get; set; }
-
-    // Inject the MarineDataRepository
-    public IndexModel(MarineDataRepository repository)
+    public class IndexModel : PageModel
     {
-        _repository = repository;
-        Message = string.Empty;
-    }
+        private readonly MarineDataRepository _repository;
 
-    public void OnGet()
-    {
-    }
+        public string Message { get; set; }
 
-    public async Task OnPostAsync(string latitude, string longitude)
-    {
-        var marineData = new MarineData
+        // Property to hold the coordinates
+        public List<MarineData> Coordinates { get; set; } = new List<MarineData>();
+
+        // Inject the MarineDataRepository
+        public IndexModel(MarineDataRepository repository)
         {
-            Latitude = double.Parse(latitude),
-            Longitude = double.Parse(longitude)
-        };
-        
-        await _repository.Create(marineData);
-        Message = "Coordinates saved successfully!";
+            _repository = repository;
+            Message = string.Empty;
+        }
+
+        public async Task OnGetAsync()
+        {
+            // Fetch all coordinates from the database
+            Coordinates = await _repository.GetAll();
+        }
+
+        public async Task OnPostAsync(string latitude, string longitude)
+        {
+            var marineData = new MarineData
+            {
+                Latitude = double.Parse(latitude),
+                Longitude = double.Parse(longitude)
+            };
+            
+            await _repository.Create(marineData);
+            Message = "Coordinates saved successfully!";
+
+            // After saving, fetch all coordinates again to update the map
+            Coordinates = await _repository.GetAll();
+        }
     }
 }
